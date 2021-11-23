8266e3c8acca5c62a6ac2bbee04d81a2cc71d8c8
angularpackage <contact@angular-package.dev>
2021-11-23 21:52:56 +0100

    refactor(Template): add usage of tags.

--- src/lib/template.class.ts
+++ src/lib/template.class.ts
@@ -1,80 +1,85 @@
-import { ResultCallback, isInstance } from '@angular-package/type';
-
-export class Template<Tpl extends string> extends String {
-
+// Type.
+import { isInstance } from '@angular-package/type';
+// Class.
+import { Tag } from '../tag/src/tag.class';
+import { Tags } from '../tag/src/tags.class';
+/**
+ *
+ */
+export class Template<
+  Tpl extends string,
+  TagNames extends string = string
+> extends String {
+  //#region properties.
   //#region static properties.
   //#region static public properties.
   /**
    * The property, with the help of `toStringTag`, changes the default tag to `'template'` for static `Template`. It can be read by the
    * `typeOf()` function of `@angular-package/type`.
    */
-   static get [Symbol.toStringTag](): string {
-    return this.#toStringTag;
+  static get [Symbol.toStringTag](): string {
+    return 'template';
   }
   //#endregion static public properties.
 
-  //#region static private properties.
-  /**
-   * The static, private (independent) property stores an instance of `Template`.
-   */
-  static #template: Template<any>;
-
-  /**
-   * The name for the `toStringTag` of `Symbol`.
-   */
-  static #toStringTag = 'template';
-
-  //#endregion static private properties.
-  //#endregion static properties.
-
   //#region instance properties.
+
+  public get tag(): Record<TagNames, {}> {
+    return this.#tags.tag;
+  }
+
+  public get tags(): Array<Tag<TagNames>> {
+    return this.#tags.tags;
+  }
   /**
    * The property, with the help of `toStringTag`, changes the default tag to `'template'` for an instance of `Template`. It can be read by
    * the `typeOf()` function of `@angular-package/type`.
    */
   public get [Symbol.toStringTag](): string {
-    return Template.#toStringTag;
+    return 'template';
   }
+
+  #tags: Tags<TagNames>;
   //#endregion instance properties.
+  //#endregion properties.
 
   //#region static methods.
-  public static define<Tpl extends string>(
-    template: Tpl,
-    callback?: ResultCallback<Tpl>
-  ): Template<Tpl> {
-    return new Template(template, callback);
-  }
-
-  public static set(tag: string): typeof Template {
-    this.#template = new Template(tag);
-    return this;
-  }
-
-  public static get<Tpl extends string>(): Template<Tpl> {
-    return this.#template;
-  }
 
   public static isTemplate<Tpl extends string = string>(
-    value: any,
-    callback?: ResultCallback<Template<Tpl>>
+    value: any
   ): value is Template<Tpl> {
-    return isInstance(value, Template, callback);
+    return isInstance(value, Template);
   }
   //#endregion static methods.
 
   //#region constructor.
-  constructor(template: Tpl | Template<Tpl>, callback?: ResultCallback<Tpl>) {
+  constructor(
+    template: Tpl,
+    ...tags: (TagNames | Tag<TagNames>)[]
+  ) {
     super(template);
+    this.#tags = new Tags<TagNames>(tags);
   }
   //#endregion constructor.
+
+  //#region instance methods.
+  public forEachTag(
+    forEachTag: (tag: Tag<TagNames>, name: TagNames) => void
+  ): this {
+    this.#tags.forEach(forEachTag);
+    return this;
+  }
+
+  public getTag<TagName extends TagNames>(name: TagName): Tag<TagName> {
+    return this.#tags.get(name) as Tag<TagName>;
+  }
+
+  public getTags(): Array<Tag<TagNames>> {
+    return this.tags;
+  }
 
   public valueOf(): Tpl {
     return super.valueOf() as Tpl;
   }
+  //#endregion instance methods.
 }
-
-// const tttt = Template.define('There is something, ');
-
-// const t = new Template('There is something wrong, believe it. [fix] ');
-// console.log(t);
-
