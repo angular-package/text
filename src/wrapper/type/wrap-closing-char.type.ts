7d45bc0561dc959bf6841eb694730eae582f9b05
angularpackage <contact@angular-package.dev>
2021-12-04 23:05:37 +0100

    refactor(Wrapper/type): remove the unnecessary types.

--- src/wrapper/type/wrap-closing-char.type.ts
+++
@@ -1,7 +0,0 @@
-// Type.
-import { StringOfLength } from '@angular-package/type';
-/**
- * The type `WrapClosingChar` of a one char `string` type indicates the closing char of the wrap. It takes the generic type variable `Chars`
- * constrained by the generic type `StringOfLength` and picks from it the second character.
- */
-export type WrapClosingChar<Chars extends StringOfLength<2, 2, string>> = Chars[1];
--- src/wrapper/type/wrap-opening-char.type.ts
+++
@@ -1,7 +0,0 @@
-// Type.
-import { StringOfLength } from '@angular-package/type';
-/**
- * The type `WrapOpeningChar` of a one char `string` type indicates the opening char of the wrap. It takes the generic type variable `Chars`
- * constrained by the generic type `StringOfLength` and picks from it the first character.
- */
-export type WrapOpeningChar<Chars extends StringOfLength<2, 2, string>> = Chars[0];
