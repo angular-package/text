d6debe5f59ac0126f0b8c087111f2e4e3f746dbe
angularpackage <contact@angular-package.dev>
2021-12-07 10:42:16 +0100

    docs(Wrap): add `@angularpackage` to the jsdoc.

--- src/wrapper/src/wrap.class.ts
+++ src/wrapper/src/wrap.class.ts
@@ -17,6 +17,7 @@ export class Wrap<
   /**
    * The `get` accessor gets the closing of the wrap.
    * @returns The return value is the wrap closing of a generic type variable `Closing`.
+   * @angularpackage
    */
   public get closing(): Closing {
     return this.#closing;
@@ -25,6 +26,7 @@ export class Wrap<
   /**
    * The `get` accessor gets the opening of the wrap.
    * @returns The return value is the wrap opening of a generic type variable `Opening`.
+   * @angularpackage
    */
   public get opening(): Opening {
     return this.#opening;
@@ -33,6 +35,7 @@ export class Wrap<
   /**
    * The `get` accessor gets the wrap consists of the opening and closing by using the **intuitive** name.
    * @returns The return value is the wrap of a generic type variable `Opening` and `Closing` on the template.
+   * @angularpackage
    */
   public get wrap(): `${Opening}${Closing}` {
     return this.value;
@@ -41,6 +44,7 @@ export class Wrap<
   /**
    * The `get` accessor gets the wrap consists of the opening and closing by using a **universal** name.
    * @returns The return value is the wrap of a generic type variable `Opening` and `Closing` on the template.
+   * @angularpackage
    */
   public get value(): `${Opening}${Closing}` {
     return this.valueOf();
@@ -50,6 +54,7 @@ export class Wrap<
    * The `get` accessor, with the help of `toStringTag`, changes the default tag to `'wrap'` for an instance of `Wrap`. It can be read by
    * the `typeOf()` function of `@angular-package/type`.
    * @returns The return value is word 'wrap` of a `string`.
+   * @angularpackage
    */
   public get [Symbol.toStringTag](): string {
     return 'wrap';
@@ -76,6 +81,7 @@ export class Wrap<
    * @param closing An optional wrap closing to check if the given `value` contains.
    * @returns The return value is a `boolean` type indicating whether the value is an instance of `Wrap` of any or given opening and
    * closing.
+   * @angularpackage
    */
   public static isWrap<Opening extends string, Closing extends string>(
     value: any,
@@ -98,6 +104,7 @@ export class Wrap<
    * @param template An array of string values where the first element is a text between opening and closing.
    * @param values A rest parameter of expressions, where the first element is the opening and the second is the closing of the wrap.
    * @returns The return value is a `string` the wrap, or an empty `string` if elements of the provided `values` are not `string`.
+   * @angularpackage
    */
   public static template(
     template: TemplateStringsArray,
@@ -120,6 +127,7 @@ export class Wrap<
    * @param closing The wrap closing of a generic type variable `Closing`.
    * @returns The return value is a new instance of `Wrap` with the primitive value of the provided `opening` and `closing` if set properly,
    * otherwise with an empty `string`.
+   * @angularpackage
    */
   constructor(opening: Opening, closing: Closing) {
     super(Wrap.template`${opening}${closing}`);
@@ -132,6 +140,7 @@ export class Wrap<
   /**
    * Gets the closing of the wrap by returning the `#closing` property of the specified object.
    * @returns The return value is the wrap closing of a generic type variable `Closing`.
+   * @angularpackage
    */
   public getClosing(): Closing {
     return this.#closing;
@@ -140,6 +149,7 @@ export class Wrap<
   /**
    * Gets the opening of the wrap by returning the `#opening` property of the specified object.
    * @returns The return value is the wrap opening of a generic type variable `Opening`.
+   * @angularpackage
    */
   public getOpening(): Opening {
     return this.#opening;
@@ -149,6 +159,7 @@ export class Wrap<
    * Gets the wrap, primitive value consists of the opening and closing by using an **intuitive** method name.
    * @returns The return value is the wrap consists of the opening and closing of a generic type variable `Opening` and `Closing` on
    * the template.
+   * @angularpackage
    */
    public getWrap(): `${Opening}${Closing}` {
     return this.valueOf();
@@ -158,6 +169,7 @@ export class Wrap<
    * Returns the wrap, primitive value of the specified `Wrap` object.
    * @returns The return value is a generic type variable `Opening` and `Closing` on the template, if properly defined, or an empty
    * `string`.
+   * @angularpackage
    */
   public valueOf(): `${Opening}${Closing}` {
     return super.valueOf() as `${Opening}${Closing}`;
