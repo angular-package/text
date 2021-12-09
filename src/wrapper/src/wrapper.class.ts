cefc3ddbacff3181657179da1b9a93aacd350896
angularpackage <contact@angular-package.dev>
2021-12-09 16:20:33 +0100

    refactor(Wrapper): a general update.
    Add
    - public static `define()` method to define a new `Wrapper`.
    - add `isTextWrapped()` method to check whether the text is wrapped.
    - add `textHasClosing()` method to check whether the text contains the closing of the wrap.
    - add `textHasOpening()` method to check whether the text contains the opening of the wrap.
    - add `unwrapText()` method to unwrap the string.
    
    Update
    - change by adding `extends Wrap` to the class to get all features from the `Wrap`.
    - change `[Symbol.toStringTag]` of instance return type to string, cause of not having an ability to extend the object.
    - change the private `#allowedChar` property to `allowedChars`.
    - change the static `getWrap()` method by replacing generic type variables.
    - change the static `isWrapper()` method by adding the `opening` and `closing` parameters.
    - change the static `setWrap()` method by replacing parameter `chars` to the `opening` and `closing`.
    - change the static `wrapText()` method by replacing the `chars` parameter with the `opening` and `closing`.
    - change constructor by replacing the `allowedChars` parameter with the `opening` and `closing` of the extended `Wrap`.
    - change the `wrapText()` method of instance to use an internal wrap to create a new instance of `Wrapped`.
    
    Remove
    - remove the static get accessor  `[Symbol.toStringTag]`.
    - remove the static method `defineWrap()`.

--- src/wrapper/src/wrapper.class.ts
+++ src/wrapper/src/wrapper.class.ts
@@ -1,29 +1,23 @@
-import { isInstance, isStringLength } from '@angular-package/type';
+import { isInstance, isStringType } from '@angular-package/type';
 // Class.
 import { AllowedChars } from '../../lib/allowed-chars.class';
 import { Wrap } from './wrap.class';
 import { Wrapped } from './wrapped.class';
 /**
- * The `Wrapper` defines the wrap in a restricted chars range and uses it to wrap specific text.
+ * The `Wrapper` is an extension of the `Wrap` object, which means it represents the immutable wrap of the opening and closing with the
+ * additional main ability to use it to wrap string. It can also define the wrap in a restricted chars range.
  */
-export class Wrapper {
+export class Wrapper<
+  Opening extends string = string,
+  Closing extends string = string
+> extends Wrap<Opening, Closing> {
   //#region properties.
   //#region static properties.
-  //#region static public properties.
-  /**
-   * The property, with the help of `toStringTag`, changes the default tag to `'wrapper'` for static `Wrapper`. It can be read by the
-   * `typeOf()` function of `@angular-package/type`.
-   */
-  public static get [Symbol.toStringTag](): 'wrapper' {
-    return 'wrapper';
-  }
-  //#endregion static public properties.
-
   //#region static private properties.
   /**
    * The private static property of the `AllowedChars` type denotes allowed characters of the wrap. The default allowed characters for the
    * wrap are [ ] ( ) < > { }. The pattern can be set by the static `setAllowedChars()` method and get by the static `getAllowedChars()`
-   * method. The pattern is used by the static `defineWrap()`, `setWrap()`, and `wrapText()` methods.
+   * method. The pattern is used by the static `define()`, `setWrap()`, and `wrapText()` methods.
    */
   private static allowedChars = new AllowedChars(/([\[\]\(\)<>{}])/g);
 
@@ -32,7 +26,7 @@ export class Wrapper {
    * and get by the static `getWrap()` method. By default, it is set to []. The property is the default value for the `wrap` parameter in
    * the `wrapText()` method of the instance.
    */
-  private static wrap: Wrap<string> = Wrapper.defineWrap('[]');
+  private static wrap: Wrap<string, string> = Wrapper.define('[', ']');
   //#endregion static private properties.
   //#endregion static properties.
 
@@ -41,62 +35,75 @@ export class Wrapper {
    * The property, with the help of `toStringTag`, changes the default tag to `'wrapper'` in the `Wrapper` instance. It can be read by the
    * `typeOf()` function of `@angular-package/type`.
    */
-  public get [Symbol.toStringTag](): 'wrapper' {
+  public get [Symbol.toStringTag](): string {
     return 'wrapper';
   }
-
-  //#region instance private properties.
-  /**
-   * The private property of the `AllowedChars` type denotes the allowed chars for `defineWrap()` and `wrapText()` method.
-   */
-  #allowedChar: AllowedChars;
-  //#endregion instance private properties.
   //#endregion instance properties.
   //#endregion properties.
 
   //#region static methods.
   /**
-   * Defines a new `Wrap` instance of two allowed chars.
-   * @param chars Two allowed chars of a generic type variable `Chars` to define a new `Wrap` instance.
-   * @returns The return value is a new `Wrap` instance of given two allowed chars.
+   * Defines a new `Wrapper` instance consisting of the allowed opening and closing.
+   * @param opening The allowed opening of the wrap of a generic type variable `Opening`.
+   * @param closing The allowed closing of the wrap of a generic type variable `Closing`.
+   * @returns The return value is a new `Wrapper` instance of given opening and closing.
+   * @angularpackage
    */
-  public static defineWrap<Chars extends string>(chars: Chars): Wrap<Chars> {
-    return new this(this.allowedChars).defineWrap(chars);
+  public static define<Opening extends string, Closing extends string>(
+    opening: Opening,
+    closing: Closing
+  ): Wrapper<Opening, Closing> {
+    return new this(
+      this.allowedChars.filterText(opening),
+      this.allowedChars.filterText(closing)
+    );
   }
 
   /**
    * Gets the allowed characters of the `AllowedChars` type from the static `Wrapper`. By default it's set to `/([\[\]\(\)<>{}])/g`.
    * The method refers to a private static property `allowedChars` which is the default value for filtering the wrap in the static
-   * `defineWrap()`, `setWrap()`, `wrapText()` methods.
+   * `define()`, `setWrap()` methods.
    * @returns The return value is an `AllowedChars` pattern of allowed characters.
+   * @angularpackage
    */
   public static getAllowedChars(): AllowedChars {
     return this.allowedChars;
   }
 
   /**
-   * Gets an instance of `Wrap` from the static `Wrapper`.
+   * Gets an instance of `Wrap` stored in the static `Wrapper`.
    * @returns The return value is an instance of `Wrap`.
+   * @angularpackage
    */
-  public static getWrap<Chars extends string>(): Wrap<Chars> {
-    return this.wrap as Wrap<Chars>;
+  public static getWrap<Opening extends string, Closing extends string>(): Wrap<
+    Opening,
+    Closing
+  > {
+    return this.wrap as Wrap<Opening, Closing>;
   }
 
   /**
    * The method checks if the value of any type is an instance of the `Wrapper`.
    * @param value The value of any type to test against the instance of `Wrapper`.
+   * @param opening An optional wrap opening to check if the given value contains.
+   * @param closing An optional wrap closing to check if the given value contains.
    * @returns The return value is a `boolean` type indicating whether the value is an instance of `Wrapper`.
+   * @angularpackage
    */
-  public static isWrapper(value: any): value is Wrapper {
-    return isInstance(value, this);
+  public static isWrapper<Opening extends string, Closing extends string>(
+    value: any,
+    opening?: Opening,
+    closing?: Closing
+  ): value is Wrapper {
+    return isInstance(value, this) && super.isWrap(value, opening, closing);
   }
 
   /**
    * The method sets the default pattern of allowed characters for static `Wrapper`. The allowed characters refer to a private static
-   * `allowedChars` property, which is the default value for filtering the wrap in the static `defineWrap()`, `setWrap()`, `wrapText()`
-   * methods.
+   * `allowedChars` property, which is the default value for filtering the wrap in the static `define()`, `setWrap()` methods.
    * @param allowedChars The allowed characters of the `RegExp` to set.
    * @returns The return value is a static `Wrapper`.
+   * @angularpackage
    */
   public static setAllowedChars(allowedChars: RegExp): typeof Wrapper {
     this.allowedChars = new AllowedChars(allowedChars);
@@ -105,67 +112,118 @@ export class Wrapper {
 
   /**
    * Sets a new instance of `Wrap` into the static `Wrapper`. The wrap set by this method is used to wrap the text by the static
-   * `wrapText()` method and the `wrapText()` method of instance by the `wrap` parameter as the default value for wrapping the text.
-   * @param chars The two allowed chars of a `string` type to set with a new instance of `Wrap`. The parameter is filtered by the allowed
-   * chars of static `Wrapper` set by the static method `setAllowedChars()`.
+   * `wrapText()` method for wrapping the text.
+   * @param opening The wrap opening of a generic type variable `Opening`.
+   * @param closing The wrap closing of a generic type variable `Closing`.
    * @returns The return value is a static `Wrapper`.
+   * @angularpackage
    */
-  public static setWrap(chars: string): typeof Wrapper {
-    this.wrap = new this(this.allowedChars).defineWrap(chars);
+  public static setWrap<Opening extends string, Closing extends string>(
+    opening: Opening,
+    closing: Closing
+  ): typeof Wrapper {
+    this.wrap = new Wrap(
+      this.allowedChars.filterText(opening),
+      this.allowedChars.filterText(closing)
+    );
     return this;
   }
 
   /**
-   * The static method wraps the specified text with a stored `Wrap` instance in the static `Wrapper` or with a given allowed `chars`.
-   * @param text The text of a generic type variable `Text` to wrap it with a stored `Wrap` in the static `Wrapper` or provided allowed
-   * `chars`.
-   * @param chars An optional two allowed chars of the `Wrap` to wrap a given text. If the value is not provided, then stored `Wrap` from
-   * the static `Wrapper` is used. The parameter is filtered by the allowed chars of static `Wrapper` set by the static method
-   * `setAllowedChars()`.
+   * The static method wraps the specified text with a stored `Wrap` instance in the static `Wrapper` or with a given opening or closing.
+   * @param text The text of a generic type variable `Text` to wrap.
+   * @param opening An optional wrap opening of a generic type variable `Opening` to wrap the given `text`. If the `opening` is not
+   * provided, then the opening from the `Wrap` instance of static `Wrapper` is used.
+   * @param closing An optional wrap closing of a generic type variable `Closing` to wrap the given `text`. If the `closing` is not
+   * provided, then the closing from the `Wrap` instance of static `Wrapper` is used.
    * @returns The return value is a new `Wrapped` instance of given `text`.
+   * @angularpackage
    */
-  public static wrapText<Text extends string, Chars extends string>(
+  public static wrapText<
+    Text extends string,
+    Opening extends string,
+    Closing extends string
+  >(
     text: Text,
-    chars?: Chars
-  ): Wrapped<Text, Chars> {
-    return isStringLength(chars, 2)
-      ? this.defineWrap(chars).wrapText(text)
-      : this.wrap.wrapText(text) as Wrapped<Text, Chars>;
+    opening: Opening = this.wrap.opening as Opening,
+    closing: Closing = this.wrap.closing as Closing
+  ): Wrapped<Text, Opening, Closing> {
+    return new this(opening, closing).wrapText(text);
   }
   //#endregion static methods.
 
   //#region constructor.
   /**
-   * Creates a new instance of `Wrapper` with the pattern of allowed chars.
-   * @param allowedChars An allowed chars of `RegExp` type to set with a new instance.
+   * Creates a new instance of `Wrapper` with the opening and closing.
+   * @param opening The wrap opening of a generic type variable `Opening`.
+   * @param closing The wrap closing of a generic type variable `Closing`.
    * @returns The return value is a new `Wrapper` instance.
+   * @angularpackage
    */
-  constructor(allowedChars: RegExp) {
-    this.#allowedChar = new AllowedChars(allowedChars);
+  constructor(opening: Opening, closing: Closing) {
+    super(opening, closing);
   }
   //#endregion constructor.
 
   //#region instance methods.
   /**
-   * Defines a new instance of `Wrap` of given allowed chars.
-   * @param chars An allowed chars of a generic type variable `Chars` to define a new instance of `Wrap`.
-   * @returns The return value is a new instance of `Wrap`of given allowed chars.
+   * The method checks if the provided `text` is wrapped with the wrap of the specified `Wrapper` object.
+   * @param text The `text` of a generic type variable `Text` to check whether it is wrapped.
+   * @returns The return value is a `boolean` indicating whether the provided `text` is wrapped.
+   * @angularpackage
    */
-  public defineWrap<Chars extends string>(chars: Chars): Wrap<Chars> {
-    return new Wrap(new AllowedChars(this.#allowedChar).filterText(chars));
+  public isTextWrapped<Text extends string>(text: Text): text is Text {
+    return this.textHasClosing(text) && this.textHasOpening(text);
   }
 
   /**
-   * Wraps the specified text with a stored `Wrap` instance in the static `Wrapper` or with a given allowed `chars`.
-   * @param text The text of a generic type variable `Text` to wrap.
-   * @param chars An optional wrap of two chars to wrap a given `text`.
-   * @returns The return value is a new `Wrapped` instance of given `text`.
+   * Checks if the provided `text` has closing of the specified `Wrapper` object.
+   * @param text The text to test against the existence of the closing.
+   * @returns The return value is a `boolean` indicating whether the given `text` has the closing of the wrap.
+   * @angularpackage
    */
-  public wrapText<Text extends string, Chars extends string>(
-    text: Text,
-    chars: Chars = Wrapper.wrap.value as Chars
-  ): Wrapped<Text, Chars> {
-    return this.defineWrap(chars).wrapText(text);
+  public textHasClosing<Text extends string>(text: Text): text is Text {
+    return (
+      isStringType(text) && text.slice(-this.closing.length) === this.closing
+    );
+  }
+
+  /**
+   * Checks if the provided `text` has the opening of the specified `Wrapper` object.
+   * @param text The text to test against the existence of the opening.
+   * @returns The return value is a `boolean` indicating whether the given `text` has the opening of the wrap.
+   * @angularpackage
+   */
+  public textHasOpening<Text extends string>(text: Text): text is Text {
+    return (
+      isStringType(text) && text.slice(0, this.opening.length) === this.opening
+    );
+  }
+
+  /**
+   * Returns the unwrapped, text from the opening and closing of the wrapper.
+   * @param text The text to unwrap.
+   * @returns The return value is the unwrapped text of a `string` if the opening or closing is found or the given text.
+   * @angularpackage
+   */
+  public unwrapText(text: string): string {
+    this.textHasClosing(text) &&
+      (text = text.valueOf().slice(0, text.length - this.closing.length));
+    this.textHasOpening(text) &&
+      (text = text.valueOf().slice(this.opening.length));
+    return text;
+  }
+
+  /**
+   * Wraps specific text with the wrap, the opening, and closing of the `Wrapper` object.
+   * @param text The text of a generic type variable `Text`, to be wrapped.
+   * @returns The return value is a new instance of the `Wrapped` type consisting of the wrapped text.
+   * @angularpackage
+   */
+  public wrapText<Text extends string>(
+    text: Text
+  ): Wrapped<Text, Opening, Closing> {
+    return new Wrapped(text, this);
   }
   //#endregion instance methods.
 }
