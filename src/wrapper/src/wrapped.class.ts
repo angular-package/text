ff4ee05aa9fb4777aa7b5b7c1fd6a447090523be
angularpackage <contact@angular-package.dev>
2021-12-10 12:13:34 +0100

    docs(Wrapped): fix missing parameter description in the static `isWrapped()` method.

--- src/wrapper/src/wrapped.class.ts
+++ src/wrapper/src/wrapped.class.ts
@@ -82,7 +82,8 @@ export class Wrapped<
   /**
    * The static method checks whether the provided `value` of any type is an instance of `Wrapped`.
    * @param value The value of any type to test against the `Wrapped` instance.
-   * @param wrap An optional opening of the `wrap` to check if the given `value` contains.
+   * @param opening An optional opening of the wrap to check if the given value contains.
+   * @param closing An optional closing of the wrap to check if the given value contains.
    * @returns The return value is a `boolean` indicating whether the value is the `Wrapped` instance of any or given chars.
    * @angularpackage
    */
