e58a4bb7a2c5825a19bf31f638792edaec9fe177
angularpackage <contact@angular-package.dev>
2021-12-15 15:36:32 +0100

    refactor(ClosingTag): change generic type variable `Chars` to `Opening` and `Closing` and put it on the template literal.

--- src/tag/type/closing-tag.type.ts
+++ src/tag/type/closing-tag.type.ts
@@ -1,9 +1,11 @@
 // Type.
 import { StringOfLength } from '@angular-package/type';
 /**
- *
+ * The generic type `ClosingTag` indicates the type of closing tag. It takes the tag name of a generic type variable `Name` constrained by
+ * the string and wrap of a generic type variable `Chars` constrained by generic type `StringOfLength` of two chars.
  */
 export type ClosingTag<
   Name extends string,
-  Chars extends StringOfLength<2, 2, string>
-> = StringOfLength<0, typeof Infinity, `${Chars[0]}/${Name}${Chars[1]}`>;
+  Opening extends string,
+  Closing extends string
+> = `${Opening}/${Name}${Closing}`;
