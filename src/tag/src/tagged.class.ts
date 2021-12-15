18eea2185d3efd520893565d3b5646c02f1972ae
angularpackage <contact@angular-package.dev>
2021-12-15 15:35:30 +0100

    refactor(Tagged): general update, change the object to contain `Opening` and `Closing` generic type variables.
    Add
    - add generic type variable `AttributeName`.
    - add accessor `value` that returns tagged text.
    - add private property `#text`.
    - add instance method `valueOf()`.
    - add static public method `template()`
    
    Change
    - reorder properties.
    - change generic type variable `Chars` to `Opening` and `Closing`.
    - change accessor `text` to refer to private property `#text`.
    - change the return type from `string` to generic type variables `Text` in the `untag()` method.
    - change the `isTagged()` method by removing the `callback` parameter.
    
    Remove
    - remove `#checkTagged()`, `#setClosingTag()`, `#setOpeningTag()`, `#isTagged()` private instance methods.

--- src/tag/src/tagged.class.ts
+++ src/tag/src/tagged.class.ts
@@ -1,39 +1,59 @@
 // @angular-package/type.
 import {
-  // Type.
-  ResultCallback,
   // Function.
-  guardString,
-  areDefined,
   isInstance,
-  isTrue,
 } from '@angular-package/type';
 // Class.
 import { Tag } from './tag.class';
 // Type.
 import { ClosingTag } from '../type/closing-tag.type';
 import { OpeningTag } from '../type/opening-tag.type';
+import { TaggedText } from '../type/tagged-text.type';
 /**
- *
+ * The `Tagged` string object represents the immutable tagged text.
  */
 export class Tagged<
   Text extends string = string,
   Name extends string = string,
-  Chars extends string = string
+  Opening extends string = string,
+  Closing extends string = string,
+  AttributeName extends string = string
 > extends String {
-  #closingTag!: ClosingTag<Name, Chars>;
-  #openingTag!: OpeningTag<Name, Chars>;
-
-  public get closingTag(): ClosingTag<Name, Chars> | undefined {
+  //#region instance accessors.
+  /**
+   * Gets the closing tag of tagged text.
+   * @returns The return value is a closing tag of tagged text of a generic type `ClosingTag`.
+   * @angularpackage
+   */
+  public get closingTag(): ClosingTag<Name, Opening, Closing> {
     return this.#closingTag;
   }
 
-  public get openingTag(): OpeningTag<Name, Chars> | undefined {
+  /**
+   * Gets the opening tag of tagged text.
+   * @returns The return value is an opening tag of tagged text of a generic type `OpeningTag`.
+   * @angularpackage
+   */
+  public get openingTag(): OpeningTag<Name, Opening, Closing> {
     return this.#openingTag;
   }
 
+  /**
+   * Gets the untagged text,
+   * @returns The return value is the text of a generic type variable `Text`.
+   * @angularpackage
+   */
   public get text(): Text {
-    return super.valueOf() as Text;
+    return this.#text;
+  }
+
+  /**
+   * Gets the tagged text.
+   * @returns The return value is the tagged text of a generic type `TaggedText`.
+   * @angularpackage
+   */
+  public get value(): TaggedText<Text, Name, Opening, Closing> {
+    return this.valueOf();
   }
 
   /**
@@ -43,52 +63,90 @@ export class Tagged<
   public get [Symbol.toStringTag](): string {
     return 'tagged';
   }
+  //#endregion instance accessors.
 
+  //#region private properties.
+  /**
+   * Private closing tag of a generic type `ClosingTag`.
+   */
+  #closingTag!: ClosingTag<Name, Opening, Closing>;
 
-  public static isTagged<Text extends string, Chars extends string>(
-    value: any,
-    callback?: ResultCallback<any>
-  ): value is Tagged<Text, Chars> {
-    return isInstance(value, Tagged, callback);
+  /**
+   * Private opening tag of a generic type `OpeningTag`.
+   */
+  #openingTag!: OpeningTag<Name, Opening, Closing>;
+
+  /**
+   * Private text of a generic type variable `Text` to be wrapped.
+   */
+  #text: Text;
+  //#endregion private properties.
+
+  //#region static methods.
+  /**
+   * Checks if the value of any type is an instance of `Tagged`.
+   * @param value The value of any to test against the `Tagged` instance.
+   * @returns The return value is a `boolean` indicating the value is an instance of `Tagged`.
+   * @angularpackage
+   */
+  public static isTagged(value: any): value is Tagged {
+    return isInstance(value, Tagged);
   }
 
-  constructor(
-    text: Text,
-    tag?: Tag<Name, Chars>,
-  ) {
-    super(guardString(text) ? text : '');
-    this.#checkTagged(tag);
+  /**
+   * The static "tag" method builds from the give parameters the tagged text of a string type on the template.
+   * @param template An array of string values where the first element is a prefix before the text.
+   * @param values A rest parameter of expressions, where the first element is text and the second an instance of `Tag`.
+   * @returns The return value is tagged text of a `string` type.
+   * @angularpackage
+   */
+  public static template(
+    template: TemplateStringsArray,
+    ...values: any[]
+  ): string {
+    let text, tag;
+    return (
+      ([text, tag] = values),
+      `${Tag.isTag(tag) ? tag.openingTag : ''}${template[0]}${text}${
+        Tag.isTag(tag) ? tag.closingTag : ''
+      }`
+    );
   }
+  //#endregion static methods.
 
-  public untag(): string {
-    return this.#isTagged()
-      ? this.text.slice(
-          this.#openingTag.length,
-          this.text.length - this.#closingTag.length
-        )
-      : this.text;
-  }
-
-  #checkTagged(tag?: Tag<Name, Chars>): this {
-    Tag.isTag(tag) && this.#setClosingTag(tag).#setOpeningTag(tag);
-    return this;
-  }
-
-  #setClosingTag(tag: Tag<Name, Chars>): this {
-    this.text.substring(
-      this.text.length - tag.closingTag.length,
-      this.text.length
-    ) === tag.closingTag && (this.#closingTag = tag.closingTag);
-    return this;
-  }
-
-  #setOpeningTag(tag: Tag<Name, Chars>): this {
-    this.text.substring(0, tag.openingTag.length) === tag.openingTag &&
+  //#region constructor.
+  /**
+   * Creates a new `Tagged` instance of given `text` tagged by the provided `tag`.
+   * @param text The text of a generic type variable `Text` to tag.
+   * @param tag The tag of instance of `Tag` to tag the given `text`.
+   * @angularpackage
+   */
+  constructor(text: Text, tag: Tag<Name, Opening, Closing, AttributeName>) {
+    super(Tagged.template`${text}${tag}`);
+    Tag.isTag(tag) &&
+      (this.#closingTag = tag.closingTag) &&
       (this.#openingTag = tag.openingTag);
-    return this;
+    this.#text = text;
+  }
+  //#endregion constructor.
+
+  //#region instance methods.
+  /**
+   * Gets the untagged text.
+   * @returns The return value is untagged text of a generic type variable `Text`.
+   * @angularpackage
+   */
+  public untag(): Text {
+    return this.#text;
   }
 
-  #isTagged(): boolean {
-    return isTrue(areDefined(this.#closingTag, this.#openingTag).every());
+  /**
+   * Gets the tagged text.
+   * @returns The return value is tagged text of a generic type `TaggedText`.
+   * @angularpackage
+   */
+  public valueOf(): TaggedText<Text, Name, Opening, Closing> {
+    return super.valueOf() as TaggedText<Text, Name, Opening, Closing>;
   }
+  //#endregion instance methods.
 }
