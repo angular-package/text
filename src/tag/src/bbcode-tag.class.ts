a4f1b2fdb88ee74e8d8f81f5ae344e679def788a
angularpackage <contact@angular-package.dev>
2021-12-16 14:25:13 +0100

    refactor(BBCodeTag): general update.
    Change:
    - change the static method name `isBBCode()` to `isBBCodeTag()`.
    - change the constructor by adding new rest parameter `attributes`  and by using the new tag proper way.
    - change the `define()` static method by adding `attributes` parameter.

--- src/tag/src/bbcode-tag.class.ts
+++ src/tag/src/bbcode-tag.class.ts
@@ -2,27 +2,33 @@
 import { isInstance, isDefined } from '@angular-package/type';
 // Class.
 import { Tag } from './tag.class';
-import { Wrap } from '../../wrapper/src/wrap.class';
 /**
- * The string object represents any tag of BBCode, a lightweight markup language.
+ * The `BBCodeTag` is an extension of `Tag` string object and represents any tag of BBCode, a lightweight markup language.
  */
-export class BBCodeTag<Name extends string> extends Tag<Name, '[]'> {
+export class BBCodeTag<
+  Name extends string,
+  AttributeName extends string = string
+> extends Tag<Name, '[', `]`, AttributeName> {
   /**
-   * The static method defines the BBCode tag of a specified name.
+   * The static method defines the BBCode tag of a specified name with optional attributes.
    * @param name The name of BBCode tag to define.
+   * @param attributes A rest parameter of array of string attribute-value pair.
    * @returns The return value is a new instance of `BBCodeTag` of the given `name`.
    */
-  public static define<Name extends string>(name: Name): BBCodeTag<Name> {
-    return new this(name);
+  public static define<Name extends string, AttributeName extends string>(
+    name: Name,
+    ...attributes: [AttributeName, string][]
+  ): BBCodeTag<Name, AttributeName> {
+    return new this(name, ...attributes);
   }
 
   /**
    * The static method checks if the value of any type is an instance of a `BBCodeTag`.
    * @param value The value of any type to check against the instance of `BBCodeTag`.
-   * @param name Optional name of a generic type variable `Name` as tag name of a given value.
+   * @param name Optional name of a generic type variable `Name`, as the tag name of a given value.
    * @returns The return value is a `boolean` type indicating whether the value is the `BBCodeTag` instance of any or a given name.
    */
-  public static isBBCode<Name extends string>(
+  public static isBBCodeTag<Name extends string>(
     value: any,
     name?: Name
   ): value is BBCodeTag<Name> {
@@ -32,10 +38,12 @@ export class BBCodeTag<Name extends string> extends Tag<Name, '[]'> {
   }
 
   /**
-   * Creates a new instance of `BBCodeTag` with a specified name.
-   * @param name The name of BBCode tag to create.
+   * Creates a new instance of `BBCodeTag` with a specified name and optional attributes.
+   * @param name The BBCode tag name of a generic type variable `Name` to create.
+   * @param attributes A rest parameter of array of string attribute-value pair.
+   * @angularpackage
    */
-  constructor(name: Name) {
-    super(name, new Wrap('[]'));
+  constructor(name: Name, ...attributes: [AttributeName, string][]) {
+    super(name, '[', ']', ...attributes);
   }
 }
