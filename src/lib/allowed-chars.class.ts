bbe74979476f5d9363fa2f897701152e8dc234d7
angularpackage <contact@angular-package.dev>
2021-12-17 16:50:00 +0100

    refactor(AllowedChars): remove static `#defaultPattern` property, remove static `define()` method, parameter `pattern` of the constructor is required, instance `filterText()` method simplified by removing generic type variable `Text`, instance method `textContains()` return type confirms the text.

--- src/lib/allowed-chars.class.ts
+++ src/lib/allowed-chars.class.ts
@@ -1,28 +1,44 @@
+// @angular-package/type
 import { guardString, isInstance } from '@angular-package/type';
-
+/**
+ * The `AllowedChars` the `RegExp` object represents the pattern of allowed chars.
+ * The constructor creates a regular expression object for matching text with a pattern.
+ */
 export class AllowedChars extends RegExp {
-  static #defaultPattern = /([\[\]\(\)<>{}])/g;
-
-  public static define(
-    pattern?: string | RegExp,
-    flags?: string | undefined
-  ): AllowedChars {
-    return new AllowedChars(pattern, flags);
-  }
-
+  /**
+   * Checks if the value is an instance of `AllowedChars`.
+   * @param value The value of any type to test against the `AllowedChars` instance.
+   * @returns The return value is a `boolean` indicating whether the value is an instance of `AllowedChars`.
+   */
   public static isAllowedChars(value: any): value is AllowedChars {
     return isInstance(value, AllowedChars);
   }
 
-  constructor(pattern?: string | RegExp, flags?: string | undefined) {
-    super(pattern || AllowedChars.#defaultPattern, flags);
+  /**
+   * Creates a new instance of `AllowedChars`.
+   * @param pattern The regular expression of a string or `RegExp` type.
+   * @param flags An optional flags of a `string` type to set.
+   * @angularpackage
+   */
+  constructor(pattern: string | RegExp, flags?: string) {
+    super(pattern, flags);
   }
 
-  public filterText<Text extends string>(text: Text): Text {
-    return (guardString(text) ? text.match(this)?.join('') : '') as Text;
+  /**
+   * Filters the text with a regular expression of a specified object.
+   * @param text The text of a `string` type to filter.
+   * @returns The return value is the filtered text if text is a string and matches was found, or an empty `string`.
+   */
+  public filterText(text: string): string {
+    return guardString(text) ? text.match(this)?.join('') || '' : '';
   }
 
-  public textContains<Text extends string>(text: Text): boolean {
+  /**
+   * Checks if the text contains allowed characters.
+   * @param text The text of a generic type variable `Text` on which perform the search.
+   * @returns The return value is a `boolean` indicating whether the text contains allowed characters.
+   */
+  public textContains<Text extends string>(text: Text): text is Text {
     return guardString(text) ? this.test(text) : false;
   }
 }
