3a5d7db46ef551cf7445e6acc288ba59890626f2
angularpackage <contact@angular-package.dev>
2021-12-20 23:48:31 +0100

    refactor(tag/index): export all.

--- src/tag/index.ts
+++ src/tag/index.ts
@@ -1,5 +1,11 @@
 export { BBCodeTag } from './src/bbcode-tag.class';
+export { ClosingTag } from './src/closing-tag.class';
 export { HtmlTag } from './src/html-tag.class';
+export { OpeningTag } from './src/opening-tag.class';
+export { TagExtension } from './src/tag-extension.class';
+// Tag.
 export { Tag } from './src/tag.class';
-export { Tagged } from './src/tagged.class';
 export { Tags } from './src/tags.class';
+// VariableTag.
+export { VariableTag } from './src/variable-tag.class';
+export { VariableTags } from './src/variable-tags.class';
