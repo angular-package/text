09e7d4add792695dbc106f525f8114798f03acb6
angularpackage <contact@angular-package.dev>
2021-12-20 23:37:00 +0100

    refactor(lib/index): add exports.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export { AllowedChars } from './allowed-chars.class';
+export { Attribute } from './attribute.class';
+export { Attributes } from './attributes.class';
 export { Template } from './template.class';
 export { Text } from './text.class';
