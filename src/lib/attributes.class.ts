36f90902a96705656a4056f1c3849f07ff485ebd
angularpackage <contact@angular-package.dev>
2021-12-21 21:14:09 +0100

    refactor(Attributes): general update and jsdoc.
    Add
    - add `valueOf()` `toString()` `has()` `getAll()` instance methods.
    - add accessor `attributes` and `value`.
    
    Change
    - change the generic type variable `Name` to `Names`.
    - change the way of building the attribute list by adding `BBCode` approach e.g. `[size=14px]`.

--- src/lib/attributes.class.ts
+++ src/lib/attributes.class.ts
@@ -1,15 +1,18 @@
 // @angular-package/type.
-import { isArray, isDefined, isFalse } from '@angular-package/type';
+import { isDefined, isFalse } from '@angular-package/type';
 // Class.
 import { Attribute } from './attribute.class';
 /**
- * The `Attributes` object is a storage for the attribute.
+ * The `Attributes` string object builds an immutable list of attributes in original insertion order where attribute comes in form
+ * `attribute="value"`.
  */
-export class Attributes<Name extends string = string> extends String {
+export class Attributes<Names extends string = string> extends String {
   /**
-   *
+   * The `get` accessor `attribute` gets the attributes in an object form, where the key is the attribute name.
+   * @returns The return value is an `object` of attributes.
+   * @angularpackage
    */
-  public get attribute(): Readonly<{ [K in Name]: string }> {
+  public get attribute(): Readonly<{ [K in Names]: string }> {
     const attributes: any = {};
     isDefined(this.#attributes) &&
       this.#attributes.forEach((attr: Attribute) =>
@@ -19,44 +22,65 @@ export class Attributes<Name extends string = string> extends String {
   }
 
   /**
-   * Private attributes of a Map.
+   * The `get` accessor returns the attributes in an array form.
+   * @returns The return value is an array of attributes.
+   * @angularpackage
    */
-  #attributes: Map<Name, Attribute<any>> = new Map();
-
+  public get attributes(): Attribute<Names>[] {
+    return Array.from(this.#attributes.values()) as Attribute<Names>[];
+  }
   /**
-   *
-   * @param template
-   * @param values
-   * @returns
+   * The `get` accessor gets the attribute list, the primitive value of a specified `Attributes` object.
+   * @returns The return value is a list of attributes of a string type built from `attribute="value""` separated by space.
+   * @angularpackage
    */
-  public static template(
-    template: TemplateStringsArray,
-    ...values: any[]
-  ): string {
-    let attributes: [string, string][];
-    const names = new Set();
-    return (
-      ([attributes] = values),
-      attributes
-        .map((attribute) =>
-          isFalse(names.has(attribute[0]))
-            ? (names.add(attribute[0]),
-              new Attribute(attribute[0], attribute[1]))
-            : undefined
-        )
-        .filter((element) => isDefined(element))
-        .join(' ')
-    );
+  public get value(): string {
+    return this.toString();
   }
 
   /**
-   *
-   * @param attributes
+   * Private attributes of a Map.
    */
-  constructor(...attributes: [Name, string][]) {
+  #attributes: Map<Names, Attribute<any>> = new Map();
+
+  /**
+   * The "tag" static method builds the attribute list.
+   * @param strings -
+   * @param values A rest parameter of attribute-value pairs of an array.
+   * @returns The return value is the attribute list of a `string` type.
+   * @angularpackage
+   */
+  protected static template<Name extends string>(
+    strings: TemplateStringsArray,
+    ...values: [Name, string][][]
+  ): string {
+    let attributes: [Name, string][];
+    const names = new Set();
+    return (
+      ([attributes] = values),
+      (attributes.length > 0 ? (attributes[0][0].length > 0 ? ` ` : ``) : ``) +
+        attributes
+          .map((attribute) =>
+            isFalse(names.has(attribute[0]))
+              ? (names.add(attribute[0]),
+                new Attribute(attribute[0], attribute[1]))
+              : undefined
+          )
+          .filter((element) => isDefined(element))
+          .join(' ')
+    );
+  }
+
+  //#region constructor.
+  /**
+   * Creates an instance of `Attributes` by specifying attribute-value pairs of an array.
+   * @param attributes A rest parameter of attribute-value pairs of an array.
+   * @angularpackage
+   */
+  constructor(...attributes: [Names, string][]) {
     super(Attributes.template`${attributes}`);
     // Add unique attributes.
-    let name: Name, value: string;
+    let name: Names, value: string;
     attributes.forEach(
       (attribute) => (
         ([name, value] = attribute),
@@ -65,15 +89,54 @@ export class Attributes<Name extends string = string> extends String {
       )
     );
   }
+  //#endregion constructor.
 
+  //#region instance public methods.
   /**
-   * Get the attribute of a specified name.
+   * Gets the attribute of a specified name.
    * @param name The name of attribute to get.
    * @returns The return value is the attribute of the `Attribute` instance.
+   * @angularpackage
    */
-  public get<AttrName extends Name>(
-    name: AttrName
-  ): Attribute<AttrName> | undefined {
+  public get<Name extends Names>(name: Name): Attribute<Name> | undefined {
     return this.#attributes.get(name);
   }
+
+  /**
+   * Gets the attributes in an array form.
+   * @returns The return value is an array of attributes.
+   * @angularpackage
+   */
+  public getAll(): Attribute<Names>[] {
+    return Array.from(this.#attributes.values()) as any;
+  }
+
+  /**
+   * Checks whether the attribute of a specified name exists in an `Attributes` object.
+   * @param name The name of attribute to check its existence.
+   * @returns The return value is the attribute of the `Attribute` instance.
+   * @angularpackage
+   */
+  public has<Name extends Names>(name: Name): boolean {
+    return this.#attributes.has(name);
+  }
+
+  /**
+   * Gets the attribute list, the primitive value of a specified `Attributes` object.
+   * @returns The return value is a list of attributes of a string type built from `attribute="value""` separated by space.
+   * @angularpackage
+   */
+  public toString(): string {
+    return super.valueOf();
+  }
+
+  /**
+   * Gets the attribute list, the primitive value of a specified `Attributes` object.
+   * @returns The return value is a list of attributes of a string type built from `attribute="value""` separated by space.
+   * @angularpackage
+   */
+  public valueOf(): string {
+    return super.valueOf() as any;
+  }
+  //#endregion instance public methods.
 }
