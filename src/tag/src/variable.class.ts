55a9dc2162f993c1d40058b3474a8777836a9d5a
angularpackage <contact@angular-package.dev>
2021-12-22 20:52:14 +0100

    refactor(Variable): change the accessor `value` type from string to the generic type variable `Value`, add accessor `variable` to get primitive value, add an optional parameter value to the static `define()` method, add `toArray()`, `toObject()`, toString()` and `valueOf()` method.

--- src/tag/src/variable.class.ts
+++ src/tag/src/variable.class.ts
@@ -3,7 +3,7 @@ import { isInstance, isDefined } from '@angular-package/type';
 // Class.
 import { Tag } from './tag.class';
 /**
- * The `VariableTag` is an extension of `Tag` string object and represents any tag of variable in the template.
+ * The `Variable` is an extension of the `Tag` string object and represents an immutable variable in the form {variable name}.
  */
 export class Variable<
   Name extends string,
@@ -11,14 +11,25 @@ export class Variable<
 > extends Tag<Name, `{`, `}`> {
   //#region instance public accessors.
   /**
-   *
+   * The `get` accessor returns the value of a specified `Variable` object.
+   * @returns The return value is the value of a `string` type if set, otherwise `undefined`.
+   * @angularpackage
    */
-  public get value(): string | undefined {
+  public get value(): Value | undefined {
     return this.#value;
   }
 
   /**
-   * The property, with the help of `toStringTag`, changes the default tag to `'bbcodeTag'` for an instance of `Tag`. It can be read by
+   * The `get` accessor returns the variable in form {variable name}, a primitive value of the specified `Variable` object.
+   * @returns The return value is the variable of a generic type variable `Name` on the template `{${Name}}`.
+   * @angularpackage
+   */
+  public get variable(): `{${Name}}` {
+    return super.valueOf();
+  }
+
+  /**
+   * The property, with the help of `toStringTag`, changes the default tag to `'variable'` for an instance of `Variable`. It can be read by
    * the `typeOf()` function of `@angular-package/type`.
    */
   public get [Symbol.toStringTag](): string {
@@ -26,30 +37,33 @@ export class Variable<
   }
   //#endregion instance public accessors.
 
+  //#region instance private properties.
   /**
-   *
+   * Private value of a generic type variable `Value`.
    */
   #value?: Value;
+  //#endregion instance private properties.
 
   //#region static public methods.
   /**
-   * The static method defines the `VariableTag` of a specified name with optional attributes.
-   * @param name The name of `VariableTag` to define.
-   * @param attributes A rest parameter of array of string attribute-value pair.
-   * @returns The return value is a new instance of `VariableTag` with the given `name`.
+   * The static method defines the `Variable` of a specified name with an optional value.
+   * @param name The name of `Variable` to define.
+   * @param value An optional value of the variable.
+   * @returns The return value is a new instance of `Variable` with the given `name` and an optional value.
    * @angularpackage
    */
-  public static define<Name extends string, AttributeName extends string>(
-    name: Name
-  ): Variable<Name> {
-    return new this(name);
+  public static define<Name extends string, Value extends string>(
+    name: Name,
+    value?: Value
+  ): Variable<Name, Value> {
+    return new this(name, value);
   }
 
   /**
-   * The static method checks if the value of any type is an instance of a `VariableTag`.
-   * @param value The value of any type to check against the instance of `VariableTag`.
-   * @param name Optional name of a generic type variable `Name`, as the tag name of a given value.
-   * @returns The return value is a `boolean` type indicating whether the value is the `VariableTag` instance of any or a given name.
+   * The static method checks if the value of any type is an instance of a `Variable`.
+   * @param value The value of any type to check against the instance of `Variable`.
+   * @param name Optional name of a generic type variable `Name`, as the variable name of a given value.
+   * @returns The return value is a `boolean` type indicating whether the value is the `Variable` instance of any or a given name.
    * @angularpackage
    */
   public static isVariable<Name extends string>(
@@ -64,8 +78,9 @@ export class Variable<
 
   //#region constructor.
   /**
-   * Creates a new instance of `VariableTag` with a specified name and optional attributes.
-   * @param name The VariableTag tag name of a generic type variable `Name` to create.
+   * Creates a new instance of `Variable` with a specified name and optional value.
+   * @param name The name of the variable in form {name} of a generic type variable `Name` to create.
+   * @param value An optional value of a variable.
    * @angularpackage
    */
   constructor(name: Name, value?: Value) {
@@ -73,18 +88,22 @@ export class Variable<
     this.#value = value;
   }
   //#endregion constructor.
+
+  //#region instance public methods.
   /**
-   *
-   * @returns
+   * Gets the value of the variable.
+   * @returns The return value is the value of a generic type variable `Value` if set, otherwise `undefined`.
+   * @angularpackage
    */
   public getValue(): Value | undefined {
     return this.#value;
   }
 
   /**
-   *
-   * @param value
-   * @returns
+   * Sets the value for the variable.
+   * @param value The value of a generic type variable `Value` to set.
+   * @returns The return value is an instance of `Variable`.
+   * @angularpackage
    */
   public setValue(value: Value): this {
     this.#value = value;
@@ -92,11 +111,49 @@ export class Variable<
   }
 
   /**
-   *
-   * @param text
-   * @returns
+   * Replaces variable in format {variable name} with the value of a specified `Variable` object in the given text.
+   * @param text The text of a `string` type in which replace the variable with the value.
+   * @returns The return value is the text of a `string` type with a replaced variable by value.
+   * @angularpackage
    */
   public replaceVariable(text: string): string {
     return this.replaceTag(text, this.#value || ``);
   }
+
+  /**
+   * Returns converted variable to array where the first element is the name, and the second is the value.
+   * @returns The return value is an array of attributes.
+   * @angularpackage
+   */
+  public toArray(): [Name, Value | undefined] {
+    return [this.name, this.#value];
+  }
+
+  /**
+   * Returns converted variable to object where the key is the variable name.
+   * @returns The return value is an object where the property name is variable name equal to variable value.
+   * @angularpackage
+   */
+  public toObject(): Readonly<{ [K in Name]: Value }> {
+    return { [this.name]: this.#value } as { [K in Name]: Value };
+  }
+
+  /**
+   * Returns the variable in form {name}, a primitive value of the specified `Variable` object.
+   * @returns The return value is the variable of a generic type variable `Name` on the template `{${Name}}`.
+   * @angularpackage
+   */
+  public toString(): `{${Name}}` {
+    return this.valueOf();
+  }
+
+  /**
+   * Returns the variable in form {name}, a primitive value of the specified `Variable` object.
+   * @returns The return value is the variable of a generic type variable `Name` on the template `{${Name}}`.
+   * @angularpackage
+   */
+  public valueOf(): `{${Name}}` {
+    return super.valueOf();
+  }
+  //#endregion instance public methods.
 }
