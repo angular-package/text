113bb1d33e5879fe2338f855506a1f5908dbf5a0
angularpackage <contact@angular-package.dev>
2021-12-22 21:51:07 +0100

    refactor(Variables): remove `tag`, `tags` accessors, change private `#tags` property to `#variables`, change constructor parameter names to include an array of name-value pair, update `delete()` method, update jsdoc.

--- src/tag/src/variables.class.ts
+++ src/tag/src/variables.class.ts
@@ -1,120 +1,128 @@
+// @angular-package/type.
+import { isStringType } from '@angular-package/type';
 // Class.
 import { Variable } from './variable.class';
 /**
- * The `tags` object creates and stores multiple variables tags.
+ * The `Variables` object creates and stores multiple variables in original insertion order.
  */
 export class Variables<Names extends string> {
+  //#region instance public accessors.
   /**
-   * Gets an `object` of set tags.
-   * @angularpackage
-   */
-  public get tag(): Record<Names, {}> {
-    return Object.fromEntries(this.#tags.entries()) as any;
-  }
-
-  /**
-   * Gets an `array` of set tags.
-   * @angularpackage
-   */
-  public get tags(): Variable<Names>[] {
-    return Array.from(this.#tags.values());
-  }
-
-  /**
-   * Gets an `object` of set tags.
+   * The `get` accessor gets an `object` of set variables where the key is the variable name.
+   * @returns The return value is an object of variables.
    * @angularpackage
    */
   public get variable(): Record<Names, {}> {
-    return Object.fromEntries(this.#tags.entries()) as any;
+    return Object.fromEntries(this.#variables.entries()) as any;
   }
 
   /**
-   * Gets an `array` of set tags.
+   * The `get` accessor gets an `array` of set variables.
+   * @returns The return value is an array of variables.
    * @angularpackage
    */
   public get variables(): Variable<Names>[] {
-    return Array.from(this.#tags.values());
+    return Array.from(this.#variables.values());
   }
 
   /**
-   * The property, with the help of `toStringTag`, changes the default tag to `'tags'` for an instance of `Tag`. It can be read by
-   * the `typeOf()` function of `@angular-package/type`.
-   * @angularpackage
+   * The property, with the help of `toStringTag`, changes the default tag to `'variables'` for an instance of `Variables`.
+   * It can be read by the `typeOf()` function of `@angular-package/type`.
    */
   public get [Symbol.toStringTag](): string {
     return 'variables';
   }
+  //#endregion instance public accessors.
 
+  //#region instance private properties.
   /**
-   *
+   * Private variables of a Map type to store `Variable` instances.
    */
-  #tags: Map<Names, Variable<any>> = new Map();
+  #variables: Map<Names, Variable<any>> = new Map();
+  //#endregion instance private properties.
 
   //#region constructor.
   /**
-   * 
-   * @param names 
+   * Creates an instance of `Variables` with a specified name, or name and value.
+   * @param names A rest parameter of string names, or an array name-value pairs to create new variables.
    * @angularpackage
    */
-  // REVIEW: 
-  // constructor(...names: (Names | [Names, string])[]) {
-  constructor(...names: Names[]) {
-    names.forEach((name) => this.#tags.set(name, new Variable(name)));
+  constructor(...names: (Names | [Names, string])[]) {
+    let name: Names, value: string;
+    names.forEach((variable) =>
+      isStringType(variable)
+        ? this.#variables.set(variable, new Variable(variable))
+        : (([name, value] = variable),
+          this.#variables.set(name, new Variable(name, value)))
+    );
   }
   //#endregion constructor.
 
-  public delete(): this {
-
-    return this;
-  }
-
+  //#region instance public methods.
   /**
-   * The method executes a provided `forEachTag` function once per each name-tag pair in the `Tags` object.
-   * @param forEach Function to execute for each entry in the `Tags`.
-   * @returns The return value is an instance of `Tags`.
+   * Removes the variable under the given name from the `Variables` storage.
+   * @param name The variable name to remove.
+   * @param removed An optional callback function to get the status of the removal.
+   * @returns The return value is an instance of `Variables`.
+   * @angularpackage
    */
-  public forEach(forEach: (variableTag: Variable<Names>) => void): this {
-    this.#tags.forEach(forEach);
+  public delete<Name extends Names>(
+    name: Name,
+    removed: (status: boolean) => void = () => {}
+  ): this {
+    removed(this.#variables.delete(name));
     return this;
   }
 
   /**
-   *
-   * @param name
-   * @returns
+   * The method executes a provided `forEach` function once per each `Variable` in the `Variables` object.
+   * @param forEach Function to execute for each entry in the `Variables`.
+   * @returns The return value is an instance of `Variables`.
+   * @angularpackage
+   */
+  public forEach(forEach: (variable: Variable<Names>) => void): this {
+    this.#variables.forEach(forEach);
+    return this;
+  }
+
+  /**
+   * The method gets the variable of a specified name.
+   * @param name The variable name to get.
+   * @returns The return value is the `Variable` instance of a specified name.
    * @angularpackage
    */
   public get<Name extends Names>(name: Name): Variable<Name> {
-    return this.#tags.get(name) as Variable<Name>;
+    return this.#variables.get(name) as Variable<Name>;
   }
 
   /**
-   *
-   * @returns
+   * Gets an array of set variables.
+   * @returns The return value is an array of variables.
    * @angularpackage
    */
   public getAll(): Variable<Names>[] {
-    return Array.from(this.#tags.values());
+    return Array.from(this.#variables.values());
   }
 
   /**
-   *
-   * @param name
-   * @returns
+   * Checks whether the variable under the given name exists.
+   * @param name The variable name to check.
+   * @returns The return value is a `boolean` indicating whether a variable of a specified name exists.
    * @angularpackage
    */
   public has<Name extends Names>(name: Name): boolean {
-    return this.#tags.has(name);
+    return this.#variables.has(name);
   }
 
   /**
-   *
-   * @param name
-   * @returns
+   * Adds or updates the variable under the given name, with an optional value.
+   * @param name The variable name to add or update.
+   * @returns The return value is an instance of `Variables`.
    * @angularpackage
    */
-  public set<Name extends Names>(name: Name): this {
-    this.#tags.set(name, new Variable(name));
+  public set<Name extends Names>(name: Name, value?: string): this {
+    this.#variables.set(name, new Variable(name, value));
     return this;
   }
+  //#endregion instance public methods.
 }
