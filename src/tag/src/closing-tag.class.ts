7d67f730a62e10606cc87c604b6e4281d3571774
angularpackage <contact@angular-package.dev>
2021-12-22 22:19:29 +0100

    refactor(ClosingTag): add accessor `toStringTag` to return `closingTag`.

--- src/tag/src/closing-tag.class.ts
+++ src/tag/src/closing-tag.class.ts
@@ -10,4 +10,12 @@ export class ClosingTag<
   constructor(name: Name, opening: Opening, closing: Closing) {
     super(name, `${opening}/`, closing);
   }
+
+  /**
+   * The `get` accessor, with the help of `toStringTag`, changes the default tag to `'closingTag'` for an instance of `ClosingTag`.
+   * It can be read by the `typeOf()` function of `@angular-package/type`.
+   */
+   public get [Symbol.toStringTag](): string {
+    return 'closingTag';
+  }
 }
