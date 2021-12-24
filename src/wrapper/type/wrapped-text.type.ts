63e2f6cf718f98db62f0b484e022a89ea168e865
angularpackage <contact@angular-package.dev>
2021-12-24 02:17:43 +0100

    refactor(WrappedText): remove unnecessary type.

--- src/wrapper/type/wrapped-text.type.ts
+++
@@ -1,8 +0,0 @@
-/**
- * The type represents the wrapped text of a generic type variables in order `Text`, `Opening` and `Closing` on the template.
- */
-export type WrappedText<
-  Text extends string,
-  Opening extends string,
-  Closing extends string
-> = `${Opening}${Text}${Closing}`;
