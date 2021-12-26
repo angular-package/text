4273317b04688763013b2c5c294a253855bcec83
angularpackage <contact@angular-package.dev>
2021-12-27 00:02:42 +0100

    test(Tags): add.

--- src/tag/test/tags.spec.ts
+++ src/tag/test/tags.spec.ts
@@ -6,7 +6,7 @@ import { Tags } from '../src/tags.class';
 const testing = new Testing(true, true);
 const toBe = new TestingToBeMatchers();
 
-testing.describe(`Tag`, () => {
+testing.describe(`Tags`, () => {
 
   const opening = `[`;
   const closing = `]`;
@@ -14,15 +14,37 @@ testing.describe(`Tag`, () => {
   const color = 'color';
   const colorValue = 'red';
 
+  const size = 'size';
+  const sizeValue = '14px';
+
   const border = 'border';
   const borderValue = '1px solid #000';
 
+  const quote = 'quote';
+  const fix = 'fix';
+  const value = 'value';
+  const problem = 'problem';
+
+  /**
+   * Simple tags.
+   */
   const tags = new Tags(
-    [`quote`, `fix`, `value`, `problem`],
+    [quote, fix, value, problem],
     opening,
     closing,
     [color, colorValue], [border, borderValue]
   );
+
+  /**
+   * Attributed tags.
+   */
+  const tagsAttributed = new Tags(
+    [quote, fix, value, problem, [size, [size, sizeValue]]],
+    opening,
+    closing,
+    [color, colorValue], [border, borderValue]
+  );
+
 
   testing
     .describe(`accessors`, () => {
@@ -40,17 +62,35 @@ testing.describe(`Tag`, () => {
 
     .describe(`methods`, () => {
       testing
+        .it(`Tags.prototype.delete()`, () => {
+          expect(tags.delete('problem').has('problem')).toBeFalse();
+          tags.set('problem');
+        })
+
         .it(`Tags.prototype.forEach()`, () => {
           tags.forEach(tag => {
             expect(tag).toBeInstanceOf(Tag);
             toBe.instance(tag, Tag);
           });
         })
+
         .it(`Tags.prototype.get()`, () => {
-          expect(tags.get('fix')).toBeInstanceOf(Tag);
-          expect(tags.get('fix').name).toEqual('fix');
-          toBe.instance(tags.get('fix'), Tag);
+          expect(tags.get(fix)).toBeInstanceOf(Tag);
+          expect(tags.get(quote)).toBeInstanceOf(Tag);
+          expect(tags.get(value)).toBeInstanceOf(Tag);
+
+          expect(tags.get(fix).name).toEqual(fix);
+          expect(tags.get(quote).name).toEqual(quote);
+          expect(tags.get(value).name).toEqual(value);
+
+          toBe
+            .instance(tags.get(fix), Tag);
+
+          expect(tagsAttributed.get(size).attribute?.border).toEqual(borderValue);
+          expect(tagsAttributed.get(size).attribute?.color).toEqual(colorValue);
+          expect(tagsAttributed.get(size).attribute?.size).toEqual(sizeValue);
         })
+
         .it(`Tags.prototype.getTags()`, () => {
           expect(tags.getTags().length).toEqual(4);
           tags.getTags().forEach(tag => {
@@ -58,54 +98,29 @@ testing.describe(`Tag`, () => {
             toBe.instance(tag, Tag);
           });
         })
+
         .it(`Tags.prototype.has()`, () => {
-          expect(tags.has('fix')).toBeTrue();
+          expect(tags.has(fix)).toBeTrue();
           toBe.true(tags.has('value'));
           toBe.false(tags.has('no tag' as any));
         })
+
         .it(`Tags.prototype.set()`, () => {
-          // expect(tags.has('fix')).toBeTrue();
-          // toBe.true(tags.has('value'));
-          // toBe.false(tags.has('value'));
+          // Remove the tag and check.
+          expect(tags.delete('problem').has('problem')).toBeFalse();
+          expect(tags.get('problem')).not.toBeInstanceOf(Tag);
+
+          // Set a new tag with attributes.
+          tags.set('problem', [color, '#fff'], [border, '1px solid #fff']);
+          expect(tags.get('problem')).toBeInstanceOf(Tag);
+          expect(tags.get('problem').attribute?.border).toEqual('1px solid #fff');
+          expect(tags.get('problem').attribute?.color).toEqual('#fff');
+
+          // Set a new tag with no attributes.
+          tags.set('problem');
+          expect(tags.get('problem')).toBeInstanceOf(Tag);
+          expect(tags.get('problem').attribute?.border).toBeUndefined();
+          expect(tags.get('problem').attribute?.color).toBeUndefined();
         });
     });
 });
-
-// //
-// tags.forEach((tag) => {});
-// tags.get('fix', );
-// tags.getTags();
-// tags.has('quote');
-// tags.set('problem', ['color', 'red'], ['font-weight', 'bold']);
-// tags.tags;
-
-// // const cc = tags.tag.fix
-
-// // console.log(tags.get('fix', 'color', 'font-weight'));
-
-// // Add tags with attributes.
-// const attributedTags = new Tags(
-//   [`quote`, `fix`, `value`, `problem`],
-//   `[`,
-//   `]`,
-//   ['color', 'red'], ['font-weight', '400']
-// );
-
-// //
-// attributedTags.forEach((tag) => {});
-// attributedTags.get('fix');
-// attributedTags.getTags();
-// attributedTags.has('quote');
-// attributedTags.set('problem', ['color', 'red'], ['font-weight', 'bold']);
-// const a = attributedTags.tags[0];
-
-// // console.log(a.valueOf());
-
-// const attributedTagsOneByOne = new Tags(
-//   [`span`, ['quote', ['font-weight', '400'], ['name', 'a']]],
-//   `[`,
-//   `]`,
-//   ['color', 'red']
-// );
-
-// console.log(attributedTagsOneByOne);
