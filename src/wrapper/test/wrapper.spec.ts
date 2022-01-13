7f0334dedc3bb20ff58a7fa966a871ba58d307de
angularpackage <contact@angular-package.dev>
2022-01-13 23:15:06 +0100

    test(Wrapper): update.

--- src/wrapper/test/wrapper.spec.ts
+++ src/wrapper/test/wrapper.spec.ts
@@ -9,10 +9,11 @@ const toBe = new TestingToBeMatchers();
 
 testing.describe(`Wrapper`, () => {
   const opening = `<`;
+  const replaceOpening = '{{';
   const closing = `>`;
+  const replaceClosing = '}}';
   const text = `There is a text to be wrapped`;
-  const wrapper = new Wrapper(opening, closing);
-  const wrap = new Wrap(opening, closing, text);
+  const wrapper = new Wrapper(opening, closing, text);
   testing
     .describe(`static methods`, () => {
       testing
@@ -37,34 +38,94 @@ testing.describe(`Wrapper`, () => {
 
           expect(Wrapper.isWrapper(wrapper, undefined, `)`)).toBeFalse();
           expect(Wrapper.isWrapper(wrapper, `(`, `)`)).toBeFalse();
-        });
+        })
+        .it(`Wrapper.replaceClosing()`, () => {
+          console.log(Wrapper.replaceClosing(text, closing, replaceClosing));
+          console.log(Wrapper.replaceOpening(text, opening, replaceOpening));
+        })
+        .it(`Wrapper.replaceOpening()`, () => {})
+        .it(`Wrapper.unwrap()`, () => {
+        })
+
+        ;
     })
+
     .describe(`instance accessors`, () => {
       testing
       .it(`[Symbol.toStringTag]`, () => {
         expect(typeOf(wrapper)).toEqual('wrapper');
       });
     })
+
     .describe(`instance methods`, () => {
       testing
-      .it(`Wrapper.prototype.isTextWrapped()`, () => {
-        expect(wrapper.isTextWrapped(`${opening}text${closing}`)).toBeTrue();
-        expect(wrapper.isTextWrapped(wrap.valueOf())).toBeTrue();
-        expect(wrapper.isTextWrapped(wrapper.wrap(text))).toBeTrue();
+
+      // .it(`Wrapper.prototype.isTextWrapped()`, () => {
+      //   expect(wrapper.isTextWrapped(`${opening}text${closing}`)).toBeTrue();
+      //   expect(wrapper.isTextWrapped(wrap.valueOf())).toBeTrue();
+      //   expect(wrapper.isTextWrapped(wrapper.wrapText(text).valueOf())).toBeTrue();
+      // })
+
+      .it(`Wrapper.prototype.isClosingIn()`, () => {
+        expect(wrapper.isClosingIn(`${opening}text${closing}`)).toBeTrue();
+        expect(wrapper.isClosingIn(`${closing}text${opening}`)).toBeFalse();
       })
-      .it(`Wrapper.prototype.textHasClosing()`, () => {
-        expect(wrapper.textHasClosing(`${opening}text${closing}`)).toBeTrue();
-        expect(wrapper.textHasClosing(`${closing}text${opening}`)).toBeFalse();
+      .it(`Wrapper.prototype.isOpeningIn()`, () => {
+        expect(wrapper.isOpeningIn(`${opening}text${closing}`)).toBeTrue();
+        expect(wrapper.isOpeningIn(`${closing}text${closing}`)).toBeFalse();
       })
-      .it(`Wrapper.prototype.textHasOpening()`, () => {
-        expect(wrapper.textHasOpening(`${opening}text${closing}`)).toBeTrue();
-        expect(wrapper.textHasOpening(`${closing}text${closing}`)).toBeFalse();
+      .it(`Wrapper.prototype.replaceClosingIn()`, () => {
+        expect(wrapper.replaceClosingIn(wrapper.value, replaceClosing)).toEqual(`${opening}${text}${replaceClosing}`);
+        expect(wrapper.replaceClosingIn(wrapper.value, replaceOpening)).not.toEqual(`${opening}${text}${replaceClosing}`);
       })
-      .it(`Wrapper.prototype.unwrapText()`, () => {
-        expect(wrapper.unwrapText(`${opening}text is ok${closing}`)).toEqual(`text is ok`);
+      .it(`Wrapper.prototype.replaceOpeningIn()`, () => {
+        expect(wrapper.replaceOpeningIn(wrapper.value, replaceOpening)).toEqual(`${replaceOpening}${text}${closing}`);
+        expect(wrapper.replaceOpeningIn(wrapper.value, replaceClosing)).not.toEqual(`${replaceOpening}${text}${closing}`);
+      })
+      .it(`Wrapper.prototype.textWrap()`, () => {
+        expect(wrapper.textWrap(replaceOpening, replaceClosing)).toEqual(`${replaceOpening}${text}${replaceClosing}`);
+        expect(wrapper.textWrap(opening, replaceClosing)).not.toEqual(`${replaceOpening}${text}${replaceClosing}`);
+        expect(wrapper.textWrap(replaceOpening, closing)).not.toEqual(`${replaceOpening}${text}${replaceClosing}`);
+      })
+      .it(`Wrapper.prototype.textReplaceClosing()`, () => {
+        expect(wrapper.textReplaceClosing(replaceOpening)).toEqual(`${opening}${text}${replaceClosing}`);
+        expect(wrapper.textReplaceClosing(opening)).not.toEqual(`${opening}${text}${replaceClosing}`);
+        expect(wrapper.textReplaceClosing(closing)).not.toEqual(`${opening}${text}${replaceClosing}`);
+      })
+      .it(`Wrapper.prototype.textReplaceOpening()`, () => {
+        expect(wrapper.textReplaceOpening(replaceOpening)).toEqual(`${replaceOpening}${text}${closing}`);
+        expect(wrapper.textReplaceOpening(opening)).not.toEqual(`${replaceOpening}${text}${closing}`);
+        expect(wrapper.textReplaceOpening(closing)).not.toEqual(`${replaceOpening}${text}${closing}`);
+      })
+      .it(`Wrapper.prototype.textUnwrap()`, () => {
+        expect(wrapper.textReplaceOpening(replaceOpening)).toEqual(`${replaceOpening}${text}${closing}`);
+        expect(wrapper.textReplaceOpening(opening)).not.toEqual(`${replaceOpening}${text}${closing}`);
+        expect(wrapper.textReplaceOpening(closing)).not.toEqual(`${replaceOpening}${text}${closing}`);
+      })
+      .it(`Wrapper.prototype.toArray()`, () => {
+        expect(wrapper.toArray()).toEqual([ opening, text, closing]);
+        expect(wrapper.toArray()).not.toEqual([ text, opening, closing]);
+      })
+      .it(`Wrapper.prototype.toWrap()`, () => {
+        expect(wrapper.toWrap().closing).toEqual(closing);
+        expect(wrapper.toWrap().opening).toEqual(opening);
+        expect(wrapper.toWrap().text).toEqual(text);
+      })
+      .it(`Wrapper.prototype.unwrap()`, () => {
+        expect(wrapper.unwrap()).toEqual(text);
       })
       .it(`Wrapper.prototype.wrap()`, () => {
-        expect(wrapper.wrap(text)).toEqual(`${opening}${text}${closing}`);
+        expect(wrapper.wrap()).toEqual(`${opening}${opening}${text}${closing}${closing}`);
+      })
+
+      .it(`Wrapper.prototype.removeWrapIn()`, () => {
+        expect(wrapper.removeWrapIn(`${opening}text is ok${closing}`)).toEqual(`text is ok`);
+        expect(wrapper.removeWrapIn(wrapper.value)).toEqual(text);
+      })
+
+      .it(`Wrapper.prototype.wrapOn()`, () => {
+        expect(wrapper.wrapOn(text)).toEqual(`${opening}${text}${closing}`);
+        expect(wrapper.wrapOn(wrapper.value)).toEqual(`${opening}${opening}${text}${closing}${closing}`);
       });
     });
 });
