942b622867bcc839fcb8ab5c0c12fb326350e188
angularpackage <contact@angular-package.dev>
2022-01-14 17:54:21 +0100

    docs(index): update comments.

--- src/tag/index.ts
+++ src/tag/index.ts
@@ -1,14 +1,13 @@
 // Attribute
 export { Attribute, Attributes } from './attribute';
-// Lib
+// Tag
 export { BBCode } from './src/bbcode.class';
 export { ClosingTag } from './src/closing-tag.class';
 export { Html } from './src/html.class';
 export { OpeningTag } from './src/opening-tag.class';
 export { TagExtension } from './src/tag-extension.class';
-
 export { Tag } from './src/tag.class';
 export { Tags } from './src/tags.class';
-
+// Variable
 export { Variable } from './variable/src/variable.class';
 export { Variables } from './variable/src/variables.class';
