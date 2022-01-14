32ae26f751955cf1d7fdbcf4dc97bb1e14ffc33a
angularpackage <contact@angular-package.dev>
2022-01-14 20:00:47 +0100

    chore(type/ClosingTag): remove unnecessary import.

--- src/tag/type/closing-tag.type.ts
+++ src/tag/type/closing-tag.type.ts
@@ -1,5 +1,3 @@
-// Type.
-import { StringOfLength } from '@angular-package/type';
 /**
  * The generic type `ClosingTag` indicates the type of closing tag. It takes the tag name of a generic type variable `Name` constrained by
  * the string and wrap of a generic type variable `Chars` constrained by generic type `StringOfLength` of two chars.
